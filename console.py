29b894ac6ed68cdb466d2d54e8626873a47a399c
alemao51092 <aarielmartinez1111@gmail.com>
2023-07-05 11:12:20 -0700

    task6

---
+++ console.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    "instantation"
+
+    prompt = "(hbnb) "
+
+    def do_help(self, arg):
+        "help function"
+        return super().do_help(arg)
+
+    def do_EOF(self, line):
+        "End Of Function"
+        return True
+
+    def do_quit(self, arg):
+        "Quit function"
+        return True
+
+    def emptyline(self):
+        "emptyline function"
+        pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
